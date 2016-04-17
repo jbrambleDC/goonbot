98f90190b86d2bbfab5c9aeddb4e7817ed7ca1df
Jordan Bramble <jordanbramble@gmail.com>
2016-04-17 18:24:35 -0400

    Reactify todo list

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -7,6 +7,14 @@ import Task from './Task.jsx';
 
 // App component - represents the whole app
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hideCompleted: false,
+    };
+  }
+
   handleSubmit(event) {
     event.preventDefault();
 
@@ -14,14 +22,24 @@ export default class App extends Component {
 
     Tasks.insert({
       text,
-      createdAt: new Date().
+      createdAt: new Date(),
     });
 
     ReactDOM.findDOMNode(this.regs.textInput).value = '';
   }
 
+  toggleHideCompleted() {
+    this.setState({
+      hideCompleted: !this.state.hideCompleted,
+    });
+  }
+
   renderTasks() {
-    return this.props.tasks.map((task) => (
+    let filteredTasks = this.props.tasks;
+    if (this.state.hideCompleted) {
+      fileredTaks = filteredTasks.filter(task => !task.checked);
+    }
+    return filteredTasks.map((task) => (
       <Task key={task._id} task={task} />
     ));
   }
@@ -31,6 +49,16 @@ export default class App extends Component {
       <div className="container">
         <header>
           <h1>Todo List</h1>
+
+          <label className="hide-completed">
+            <input
+              type="checkbox"
+              readOnly
+              checked={this.state.hideCompleted}
+              onClick={this.toggleHideCompleted.bind(this)}
+            />
+            Hide Completed Tasks
+          </label>
 
           <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
             <input
@@ -56,6 +84,7 @@ App.proptypes = {
 export default createContainer(() => {
   return {
     tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
+    incompleteCount: PropTypes.number.isRequired,
   };
 }, App);
 
